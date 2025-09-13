459892eb45a2ba1242f43396f0eb3a16d229e345
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-13 18:35:12 +0530

    rdm update

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -6,7 +6,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({"bluz71/vim-moonfly-colors", {
+tequire("lazy").setup({"bluz71/vim-moonfly-colors", {
     'saghen/blink.cmp',
     dependencies = {'rafamadriz/friendly-snippets'},
     version = '1.*',
