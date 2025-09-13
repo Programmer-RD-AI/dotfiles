764d3be2b4b07e83d40c1b574c3f84cfbc6b52f9
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-13 18:49:29 +0530

    update tab spacing default and for web dev

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -6,9 +6,17 @@ vim.opt.smartcase = false
 vim.opt.hlsearch = false
 vim.opt.wrap = true
 vim.opt.breakindent = true
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 2
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.termguicolors = true
-vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
 vim.opt.clipboard = 'unnamedplus'
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "javascript", "typescript", "json", "yaml", "html", "css" },
+  callback = function()
+    vim.opt_local.tabstop = 2
+    vim.opt_local.shiftwidth = 2
+  end,
+})
