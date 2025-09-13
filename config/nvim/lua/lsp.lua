212641203c0cde9001057bc36705af4c882db8b8
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-13 22:41:58 +0530

    formatt

--- config/nvim/lua/lsp.lua
+++ config/nvim/lua/lsp.lua
@@ -1,147 +1,152 @@
-require('mason').setup({
-    ui = {
-        icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
-        }
-    }
+require("mason").setup({
+	ui = {
+		icons = {
+			package_installed = "✓",
+			package_pending = "➜",
+			package_uninstalled = "✗",
+		},
+	},
 })
 
-require('mason-lspconfig').setup({
-    ensure_installed = {'pyright', 'lua_ls', 'rust_analyzer', 'ts_ls', 'gopls'}
+require("mason-lspconfig").setup({
+	ensure_installed = {
+		"pyright",
+		"lua_ls",
+		"rust_analyzer",
+		"ts_ls",
+		"gopls",
+	},
 })
 
-local lspconfig = require('lspconfig')
+local lspconfig = require("lspconfig")
 
 local capabilities = require("blink.cmp").get_lsp_capabilities()
 
-
 local signs = {
-  Error = " ",
-  Warn  = " ",
-  Hint  = " ",
-  Info  = " ",
+	Error = " ",
+	Warn = " ",
+	Hint = " ",
+	Info = " ",
 }
 
 for t, icon in pairs(signs) do
-  local hl = "DiagnosticSign" .. t
-  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+	local hl = "DiagnosticSign" .. t
+	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 local on_attach = function(_, bufnr)
-    -- Enable completion triggered by <c-x><c-o>
-    vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+	-- Enable completion triggered by <c-x><c-o>
+	vim.api.nvim_buf_set_option(bufnr, "omnifunc", "v:lua.vim.lsp.omnifunc")
 
-    -- See `:help vim.lsp.*` for documentation on any of the below functions
-    local bufopts = {
-        noremap = true,
-        silent = true,
-        buffer = bufnr
-    }
+	-- See `:help vim.lsp.*` for documentation on any of the below functions
+	local bufopts = {
+		noremap = true,
+		silent = true,
+		buffer = bufnr,
+	}
 
-    -- Core navigation keymaps
-    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, bufopts)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, bufopts)
-    vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, bufopts)
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, bufopts)
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, bufopts)
-    vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, bufopts)
+	-- Core navigation keymaps
+	vim.keymap.set("n", "gD", vim.lsp.buf.declaration, bufopts)
+	vim.keymap.set("n", "gd", vim.lsp.buf.definition, bufopts)
+	vim.keymap.set("n", "gi", vim.lsp.buf.implementation, bufopts)
+	vim.keymap.set("n", "gr", vim.lsp.buf.references, bufopts)
+	vim.keymap.set("n", "K", vim.lsp.buf.hover, bufopts)
+	vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, bufopts)
 
-    -- Workspace management
-    -- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
-    -- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
-    -- vim.keymap.set('n', '<space>wl', function()
-    --     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-    -- end, bufopts)
+	-- Workspace management
+	-- vim.keymap.set('n', '<space>wa', vim.lsp.buf.add_workspace_folder, bufopts)
+	-- vim.keymap.set('n', '<space>wr', vim.lsp.buf.remove_workspace_folder, bufopts)
+	-- vim.keymap.set('n', '<space>wl', function()
+	--     print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+	-- end, bufopts)
 
-    -- Type and refactoring
-    vim.keymap.set('n', '<space>D', vim.lsp.buf.type_definition, bufopts)
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, bufopts)
-    vim.keymap.set('n', '<space>ca', vim.lsp.buf.code_action, bufopts)
-    vim.keymap.set("n", "<space>fo", function()
-        vim.lsp.buf.format({
-            async = true
-        })
-    end, bufopts)
+	-- Type and refactoring
+	vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, bufopts)
+	vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, bufopts)
+	vim.keymap.set("n", "<space>ca", vim.lsp.buf.code_action, bufopts)
+	vim.keymap.set("n", "<space>fo", function()
+		vim.lsp.buf.format({
+			async = true,
+		})
+	end, bufopts)
 
-    vim.api.nvim_create_autocmd("BufWritePre", {
-        buffer = bufnr,
-        callback = function()
-            vim.lsp.buf.format({ async = false })
-        end,
-    })
+	vim.api.nvim_create_autocmd("BufWritePre", {
+		buffer = bufnr,
+		callback = function()
+			vim.lsp.buf.format({ async = false })
+		end,
+	})
 end
 
--- Python with pyright 
+-- Python with pyright
 lspconfig.pyright.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        python = {
-            analysis = {
-                autoSearchPaths = true,
-                useLibraryCodeForTypes = true,
-                typeCheckingMode = "basic"
-            }
-        }
-    }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		python = {
+			analysis = {
+				autoSearchPaths = true,
+				useLibraryCodeForTypes = true,
+				typeCheckingMode = "basic",
+			},
+		},
+	},
 })
 
 -- TypeScript/JavaScript
 lspconfig.ts_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities
+	on_attach = on_attach,
+	capabilities = capabilities,
 })
 
 -- Go
 lspconfig.gopls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        gopls = {
-            gofumpt = true,
-            analyses = {
-                unusedparams = true,
-            },
-            staticcheck = true,
-        }
-    }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		gopls = {
+			gofumpt = true,
+			analyses = {
+				unusedparams = true,
+			},
+			staticcheck = true,
+		},
+	},
 })
 
 -- Rust
 lspconfig.rust_analyzer.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        ["rust-analyzer"] = {
-            checkOnSave = {
-                command = "clippy"
-            }
-        }
-    }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		["rust-analyzer"] = {
+			checkOnSave = {
+				command = "clippy",
+			},
+		},
+	},
 })
 
 -- Lua
 lspconfig.lua_ls.setup({
-    on_attach = on_attach,
-    capabilities = capabilities,
-    settings = {
-        Lua = {
-            runtime = {
-                version = 'LuaJIT'
-            },
-            diagnostics = {
-                globals = {'vim'}
-            },
-            workspace = {
-                library = vim.api.nvim_get_runtime_file("", true)
-            },
-            telemetry = {
-                enable = false
-            }
-        }
-    }
+	on_attach = on_attach,
+	capabilities = capabilities,
+	settings = {
+		Lua = {
+			runtime = {
+				version = "LuaJIT",
+			},
+			diagnostics = {
+				globals = { "vim" },
+			},
+			workspace = {
+				library = vim.api.nvim_get_runtime_file("", true),
+			},
+			telemetry = {
+				enable = false,
+			},
+		},
+	},
 })
 
 -- -- Customized on_attach function
