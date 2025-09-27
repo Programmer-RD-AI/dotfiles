30955db9c6e7a35d5b531ac118797adad180850a
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-09-27 10:04:40 +0530

    Formating Changes

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -1,9 +1,11 @@
 function getPythonPath()
-        local root = vim.fs.dirname(vim.fs.find({ "pyproject.toml", "setup.py", ".git" }, { upward = true })[1])
-        local venv = root .. "/.venv"
-        local python = venv .. "/bin/python"
-        if vim.loop.os_uname().sysname:match("Windows") then
-                python = venv .. "\\Scripts\\python.exe"
-        end
-        return python
+	local root = vim.fs.dirname(
+		vim.fs.find({ "pyproject.toml", "setup.py", ".git" }, { upward = true })[1]
+	)
+	local venv = root .. "/.venv"
+	local python = venv .. "/bin/python"
+	if vim.loop.os_uname().sysname:match("Windows") then
+		python = venv .. "\\Scripts\\python.exe"
+	end
+	return python
 end
