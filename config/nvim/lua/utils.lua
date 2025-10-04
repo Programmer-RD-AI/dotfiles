e746b4ebc2a408c158bc9e13efbc9344c555674e
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-10-04 18:49:39 +0530

    Formatting

--- config/nvim/lua/utils.lua
+++ config/nvim/lua/utils.lua
@@ -2,7 +2,9 @@ local M = {}
 local util = require("lspconfig.util")
 
 function M.getPythonPath()
-	local root = vim.fs.dirname(vim.fs.find({ "pyproject.toml", "setup.py", ".git" }, { upward = true })[1])
+	local root = vim.fs.dirname(
+		vim.fs.find({ "pyproject.toml", "setup.py", ".git" }, { upward = true })[1]
+	)
 	local venv = root .. "/.venv"
 	local python = venv .. "/bin/python"
 	if vim.loop.os_uname().sysname:match("Windows") then
@@ -12,7 +14,12 @@ function M.getPythonPath()
 end
 
 function M.root_dir(fname)
-	return util.root_pattern("ruff.toml", ".ruff.toml", "pyproject.toml", ".git")(fname) or util.path.dirname(fname)
+	return util.root_pattern(
+		"ruff.toml",
+		".ruff.toml",
+		"pyproject.toml",
+		".git"
+	)(fname) or util.path.dirname(fname)
 end
 
 return M
