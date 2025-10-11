77a569f9b4c670b8716615391bf31ff027e8ca95
Programmer-RD-AI <79456372+Programmer-RD-AI@users.noreply.github.com>
2025-10-11 16:43:46 +0530

    chore: bugfix

--- config/wezterm/wezterm.lua
+++ config/wezterm/wezterm.lua
@@ -63,6 +63,7 @@ config.keys = { -- Enter CopyMode
 		key = "[",
 		mods = "LEADER",
 		action = act.ActivateCopyMode,
+		{ key = "Enter", mods = "SHIFT", action = wezterm.action({ SendString = "\x1b\r" }) },
 	},
 	{
 		key = "Enter",
@@ -136,9 +137,9 @@ config.keys = { -- Enter CopyMode
 	{ key = "v", mods = "CTRL|SHIFT", action = act.PasteFrom("Clipboard") },
 
 	-- Font resizing
-	{ key = "=", mods = "CTRL", action = act.IncreaseFontSize },
-	{ key = "-", mods = "CTRL", action = act.DecreaseFontSize },
-	{ key = "0", mods = "CTRL", action = act.ResetFontSize },
+	{ key = "=", mods = "CTRL",       action = act.IncreaseFontSize },
+	{ key = "-", mods = "CTRL",       action = act.DecreaseFontSize },
+	{ key = "0", mods = "CTRL",       action = act.ResetFontSize },
 }
 
 -- CopyMode key table
@@ -272,7 +273,7 @@ config.key_tables = {
 			action = act.CopyMode("MoveToStartOfLineContent"),
 		},
 		{ key = "b", mods = "NONE", action = act.CopyMode("MoveBackwardWord") },
-		{ key = "b", mods = "ALT", action = act.CopyMode("MoveBackwardWord") },
+		{ key = "b", mods = "ALT",  action = act.CopyMode("MoveBackwardWord") },
 		{ key = "b", mods = "CTRL", action = act.CopyMode("PageUp") },
 		{
 			key = "d",
@@ -289,7 +290,7 @@ config.key_tables = {
 			mods = "NONE",
 			action = act.CopyMode({ JumpForward = { prev_char = false } }),
 		},
-		{ key = "f", mods = "ALT", action = act.CopyMode("MoveForwardWord") },
+		{ key = "f", mods = "ALT",  action = act.CopyMode("MoveForwardWord") },
 		{ key = "f", mods = "CTRL", action = act.CopyMode("PageDown") },
 		{
 			key = "g",
@@ -335,7 +336,7 @@ config.key_tables = {
 			mods = "NONE",
 			action = act.CopyMode("MoveForwardWord"),
 		},
-		{ key = "PageUp", mods = "NONE", action = act.CopyMode("PageUp") },
+		{ key = "PageUp",   mods = "NONE", action = act.CopyMode("PageUp") },
 		{ key = "PageDown", mods = "NONE", action = act.CopyMode("PageDown") },
 		{
 			key = "End",
@@ -363,7 +364,7 @@ config.key_tables = {
 			mods = "ALT",
 			action = act.CopyMode("MoveForwardWord"),
 		},
-		{ key = "UpArrow", mods = "NONE", action = act.CopyMode("MoveUp") },
+		{ key = "UpArrow",   mods = "NONE", action = act.CopyMode("MoveUp") },
 		{ key = "DownArrow", mods = "NONE", action = act.CopyMode("MoveDown") },
 	},
 }
