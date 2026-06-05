4296c5d9e80a769b267df85be426bbaec9ff0bbe
prdai <prdai@users.noreply.github.com>
2026-06-05 07:08:29 +0530

    chore: so far changes
    ref: https://github.com/ThePrimeagen/harpoon/issues/662

--- nvim/lua/utils.lua
+++ nvim/lua/utils.lua
@@ -1,22 +1,13 @@
-local M = {}
+local H = {};
 
-function M.getPythonPath()
-	local root = vim.fs.dirname(
-		vim.fs.find({ "pyproject.toml", "setup.py", ".git" }, { upward = true })[1]
-	)
-	local venv = root .. "/.venv"
-	local python = venv .. "/bin/python"
-	if vim.loop.os_uname().sysname:match("Windows") then
-		python = venv .. "\\Scripts\\python.exe"
+function H.get_harpoon_key()
+	local branch = vim.fn.system("git branch --show-current")
+	local cwd = vim.loop.cwd()
+	if branch then
+		cwd = cwd .. "::" .. branch
 	end
-	return python
+	return cwd
 end
 
-function M.root_dir(fname)
-	local markers = { "ruff.toml", ".ruff.toml", "pyproject.toml", ".git" }
-	local root = vim.fs.root(fname or 0, markers)
-	return root
-		or vim.fn.fnamemodify(fname or vim.api.nvim_buf_get_name(0), ":p:h")
-end
 
-return M
+return H
